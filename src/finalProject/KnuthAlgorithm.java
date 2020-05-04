e128d19df6ca5a963ff8535b89f1fc5906216ecb
msimbao <msimbao@macalester.edu>
2020-05-04 14:56:55 -0500

    Finally Setup The Proper Repo.

---
+++ src/finalProject/KnuthAlgorithm.java
@@ -0,0 +1,66 @@
+package finalProject;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class KnuthAlgorithm {
+
+
+    public class DonaldKnuth implements BreakCode {
+
+        private Pegs response = null;
+        private Code guess = null;
+
+        private final List<Code> impossible = new LinkedList<>();
+        private final Set<Code> possible;
+
+        public DonaldKnuth() {
+            possible = Code.getRange();
+        }
+
+        public void receiveKey(Pegs peg) {
+            response = peg;
+            Iterator<Code> iterator = possible.iterator();
+            while (iterator.hasNext()) {
+                Code i = iterator.next();
+                if (!guess.getKey(i).equals(response)) {
+                    impossible.add(i);
+                    iterator.remove();
+                }
+            }
+        }
+
+
+        public Code guess() {
+            int minimumEliminated = -1;
+            Code bestGuess = null;
+            List<Code> unused = new LinkedList<>(possible);
+            unused.addAll(impossible);
+            for (Code a : unused) {
+                int[][] minMaxTable = new int[Code.getCodeLength() + 1][Code.getCodeLength() + 1];
+                for (Code b : possible) {
+                    Pegs abKey = a.getKey(b);
+                    minMaxTable[abKey.getBlack()][abKey.getWhite()]++;
+                }
+                int mostHits = -1;
+                for (int[] row : minMaxTable) {
+                    for (int i : row) {
+                        mostHits = Integer.max(i, mostHits);
+                    }
+                }
+                int score = possible.size() - mostHits;
+                if (score > minimumEliminated) {
+                    minimumEliminated = score;
+                    bestGuess = a;
+                }
+            }
+            guess = bestGuess;
+            System.out.println("Enter secret code: " + guess);
+            return bestGuess;
+        }
+
+
+    }
+}
---
+++ src/finalProject/Pegs.java
@@ -0,0 +1,70 @@
+package finalProject;
+
+public class Pegs {
+
+        private final int black;
+        private final int white;
+        private final int none;
+
+        public Pegs(int black, int white, int none) {
+            this.black = black;
+            this.white = white;
+            this.none = none;
+        }
+
+        public boolean isCorrect() {
+            return white == 0 && none == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 13 * hash + this.black;
+            hash = 13 * hash + this.white;
+            hash = 13 * hash + this.none;
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final Pegs other = (Pegs) obj;
+            if (this.black != other.black) {
+                return false;
+            }
+            if (this.white != other.white) {
+                return false;
+            }
+            if (this.none != other.none) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "black=" + black + ", white=" + white + ", none=" + none;
+        }
+
+        public int getBlack() {
+            return black;
+        }
+
+        public int getWhite() {
+            return white;
+        }
+
+        public int getNone() {
+            return none;
+        }
+
+
+}
